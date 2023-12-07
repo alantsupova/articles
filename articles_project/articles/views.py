b06b491329dfd0cecc606cf32e6cdcc606dfc52a
aleksandra <aleksaantsupova@yandex.ru>
2023-12-07 17:29:18 +0300

    .

--- articles_project/articles/views.py
+++ articles_project/articles/views.py
@@ -33,5 +33,12 @@ class KeyWordsCreateView(CreateView):
 
 class ArticleListView(ListView):
     template_name = 'article-list.html'
-    model = Article
     context_object_name = 'articles'
+
+    def get_queryset(self):
+        return Article.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
